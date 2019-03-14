b2f4387dae63b97961f18fdf96a4136908ece777
Alexis Mangin <alexis.mangin@zopa.com>
2019-03-14 11:47:13 +0000

    Initial commit

---
+++ App.js
@@ -0,0 +1,36 @@
+// In App.js in a new project
+
+import React from "react";
+import { View, Text, Button } from "react-native";
+import { createStackNavigator, createAppContainer } from "react-navigation";
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Home Screen</Text>
+        <Button
+          title="Details"
+          onPress={() => this.props.navigation.navigate("Details")}
+        />
+      </View>
+    );
+  }
+}
+
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Details Screen</Text>
+      </View>
+    );
+  }
+}
+
+const AppNavigator = createStackNavigator({
+  Home: HomeScreen,
+  Details: DetailsScreen
+});
+
+export default createAppContainer(AppNavigator);
